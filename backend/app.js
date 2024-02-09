2aff6f69bdd8bdb8b77dd58457e8e960e6dc91af
Kaustubh <pro.vaidya4413@gmail.com>
2024-02-09 23:48:17 +0530

    app.js running

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`server running at port ${PORT}`);
+})
+
+
